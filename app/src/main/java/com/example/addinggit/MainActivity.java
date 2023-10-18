007c273d94c8d66d69768820e37597fdc14eb2c2
HebaASayed <hebaahmedsayed77@gmail.com>
2023-10-18 12:07:49 +0300

    add Toast to MainActivity

--- app/src/main/java/com/example/addinggit/MainActivity.java
+++ app/src/main/java/com/example/addinggit/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.addinggit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "try change", Toast.LENGTH_SHORT).show();
     }
 }
